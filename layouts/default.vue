40f6f292248b39eb7ea327819bc6a60629b77b33
yoichiro-snbn05 <yoichiro@example.com>
2024-10-21 06:40:11 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app>
+    <v-content>
+      <Nuxt />
+    </v-content>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        hoge
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {};
+</script>
